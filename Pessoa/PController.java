b8c96d58a93c34155d9d37d47aabc828089cab34
Guilherme Soster <guilhermesosters@gmail.com>
2014-03-22 14:24:38 -0300

    Adicionando formas de criar numero random sem ser utilizando a classe Math.

--- Pessoa/PController.java
+++ Pessoa/PController.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Random;
 public class PController{
     //Atributos
@@ -14,14 +15,63 @@ public class PController{
         "Rua Andrade Neves", "Av. Salvatorri Trulliano", "Av Porca Maddona", "Rua Gari", "St. San Martin", "St. Correa", "Rua Rio de Janeiro", "Av. Rio Grande",//8
         "Rua Sao Paulo", "Rua San Sebastian", "Av. Pallermo", "Av. Zumbi dos Palmares", "Av. Isaura","Rua Mathias"};//6
     
-    //MÃ©todo Construtor    
+    //Metodo Construtor    
     public PController(){
         this.pv = new PView();
     }
-        
+     
+    /*
+     * para que o método fique o mais próximo de ser aleatório possível, o mesmo receberá um valor que multiplicará pelo resultado.
+     */
+    private int gerarRandom(int val){    	
+    	//primeiro se pega os segundos da hora atual do sistema
+    	Calendar data = Calendar.getInstance(); 
+    	int segundos = data.get(Calendar.SECOND);
+    	String seg;
+    	//converte o valor interger em String concatenando ele com uma string
+    	seg = segundos + "i";
+    	//separamos os valores dos segundos
+    	String sn1 = seg.substring(0,1);
+    	String sn2 = seg.substring(1,2);
+    	int n1 = Integer.parseInt(sn1);
+    	int n2 = Integer.parseInt(sn2);
+    	int nRandom = 0;    	
+    	if((n1!=0) && (n2!=0)){
+    		//caso o valor recebido seja par, faremos com que multiplique pelo n1
+    		if(val%2 == 0){
+    			n1 = n1*val;    		
+    		}else{ //se o valor do parâmetro for impar, multiplicaremos o n2 por ele.
+    			n2 = n2*val;    			
+    		}
+    		nRandom = n1*n2;    		    	
+    	}
+    	//como o array só contem 20 valores, o indice não pode ser superior a 20 (0-19)
+    	while(nRandom > 20){
+			nRandom -= 3;
+		}
+    	return nRandom;
+    }
+    
     public String escolhe(int tipo){
-        Random r = new Random();  
-        int nr = r.nextInt(20);  
+    	//Pega os milesegundos da data, e os soma (não multiplica para não zerar, caso algum seja zero).
+    	int parametro = 0;
+    	do{
+	    	Calendar data = Calendar.getInstance();
+	    	int mils = data.get(Calendar.MILLISECOND);
+	    	String milseg = mils + "i";
+	    	String sn1 =  milseg.substring(0,1);
+	    	String sn2 =  milseg.substring(1,2);
+	    	int n1 = Integer.parseInt(sn1);
+	    	int n2 = Integer.parseInt(sn2);	    	
+	    	if(n1 != 0){
+	    		parametro += n1;
+	    	}
+	    	if(n2 != 0){
+	    		parametro += n2;
+	    	}
+    	}while(parametro == 0);
+    	//para garantir a aleatoridade do número, será passado um parâmetro (que é a soma dos milisegundos da data)        	
+    	int nr = this.gerarRandom(parametro);    	
         if(tipo == 1){
             return this.listaNomes[nr];
         }else if(tipo == 0){
